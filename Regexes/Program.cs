23db21a95f301e93310fb181bc937561fb47e49b
c1berk0tleta <artyom.vl.grechko@gmail.com>
2022-09-28 17:18:08 +0300

    Добавьте файлы проекта.

---
+++ Regexes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Regexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Завтрак в 09:00";
+            Time(text);
+        }
+
+        public static void Time(string text)
+        {
+            Regex regex = new Regex("([0-1][0-9]|2[0-3]):[0-5][0-9]");
+            MatchCollection match = regex.Matches(text);
+
+            if (match.Count > 0)
+            {
+                foreach (Match m in match)
+                    Console.WriteLine(m);
+            }
+            else
+            {
+                Console.WriteLine("Задано неправильное время");
+            }
+        }
+    }
+}
